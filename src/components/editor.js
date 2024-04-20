4b274cda283835296278c29e42a4d25ce77fca93
Clyde D'Souza <clyd94@gmail.com>
2024-04-20 16:38:20 +1200

    Remove console error re for attribute

--- src/components/editor.js
+++ src/components/editor.js
@@ -55,7 +55,7 @@ function Editor() {
   return (
     <div id="editor" className="row">
       <div>
-        <label for="watermarkText">Watermark text</label>
+        <label htmlFor="watermarkText">Watermark text</label>
         <input
           type="text"
           value={watermarkText}
@@ -65,7 +65,7 @@ function Editor() {
       </div>
 
       <div>
-        <label for="color">Watermark color</label>
+        <label htmlFor="color">Watermark color</label>
         <input
           type="color"
           value={color}
@@ -75,7 +75,7 @@ function Editor() {
       </div>
 
       <div>
-        <label for="fontsize">Font size</label>
+        <label htmlFor="fontsize">Font size</label>
         <input
           type="text"
           value={fontSize}
@@ -85,7 +85,7 @@ function Editor() {
       </div>
 
       <div>
-        <label for="top">Top position</label>
+        <label htmlFor="top">Top position</label>
         <input
           type="text"
           value={topPosition}
@@ -95,7 +95,7 @@ function Editor() {
       </div>
 
       <div>
-        <label for="left">Left position</label>
+        <label htmlFor="left">Left position</label>
         <input
           type="text"
           value={leftPosition}
@@ -105,7 +105,7 @@ function Editor() {
       </div>
 
       <div>
-        <label for="font">Font style</label>
+        <label htmlFor="font">Font style</label>
         <FontPicker
           apiKey="AIzaSyAiWXiHsvjd7BO-U7cI3c_bn6x__yK1FfY"
           activeFontFamily={fontFamily}
