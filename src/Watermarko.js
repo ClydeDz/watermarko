72e5fac5b5346cffb8b22342f0cd6fe4f81963cf
Clyde D'Souza <clyd94@gmail.com>
2024-04-20 21:18:16 +1200

    Testing out libraries

--- src/Watermarko.js
+++ src/Watermarko.js
@@ -3,6 +3,9 @@ import "./Watermarko.css";
 import Preview from "./components/preview";
 import Editor from "./components/editor";
 
+// https://html2canvas.hertzen.com/
+// http://fabricjs.com/
+
 function Watermarko() {
   return (
     <div className="container">
--- src/components/hiddenImages.js
+++ src/components/hiddenImages.js
@@ -1,13 +1,44 @@
-function HiddenImages () {
-    return (
-        <>
-            <div id="images" style={{visibility: 'hidden'}}>      
-                <img id="watermakedImageWithText"/>
-            </div> 
-            <img id="originalImage" style={{display: 'none'}} />
-            <a id="download" style={{display: 'none'}} ></a>
-        </>
-    );
+import Moveable from "react-moveable";
+import * as React from "react";
+
+function HiddenImages() {
+  const [xyz, setxyz] = React.useState("aaa");
+
+  return (
+    <>
+      <div id="images" style={{ visibility: "hidden" }}>
+        <div className="target">
+          <input
+            type="text"
+            value={xyz}
+            onChange={(e) => setxyz(e.target.value)}
+          />
+        </div>
+
+        <Moveable
+          target={".target"}
+          draggable={true}
+          scalable={true}
+          warpable={true}
+          checkInput={true}
+          dragContainer={".targetbob"}
+          renderDirections={true}
+          // onScaleStart={(e) => {
+          //   e.setMinScaleSize([100, 100]);
+          //   e.setMaxScaleSize([500, 500]);
+          // }}
+          onRender={(e) => {
+            console.log(e);
+            e.target.style.cssText += e.cssText;
+          }}
+        />
+        <img id="watermakedImageWithText" className="targetbob" />
+      </div>
+
+      <img id="originalImage" style={{ display: "none" }} />
+      <a id="download" style={{ display: "none" }}></a>
+    </>
+  );
 }
 
 export default HiddenImages;
