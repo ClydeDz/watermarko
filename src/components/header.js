477afccc6c584616ea49ef04e66cc4b41c166c63
Clyde D'Souza <clyd94@gmail.com>
2024-04-25 19:51:05 +1200

    Adjust size of header

--- src/components/header.js
+++ src/components/header.js
@@ -6,8 +6,10 @@ export default function Header() {
   return (
     <div id="header">
       <h1>Watermarko</h1>
-      <License />
-      <Download />
+      <div>
+        <License />
+        <Download />
+      </div>
     </div>
   );
 }
