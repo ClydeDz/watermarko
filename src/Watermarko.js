087c08ee0e60e67e376c6f0930f2b774b7e05a24
Clyde D'Souza <clyd94@gmail.com>
2024-05-19 19:16:45 +1200

    Add responsive banner note

--- src/Watermarko.js
+++ src/Watermarko.js
@@ -1,12 +1,14 @@
 import Preview from "./components/preview";
 import Header from "./components/header";
 import Toolbar from "./components/toolbar";
+import { ResponsiveBanner } from "./components/ResponsiveBanner";
 
 function Watermarko() {
   return (
     <>
       <Header />
       <Toolbar />
+      <ResponsiveBanner />
       <Preview />
     </>
   );
---
+++ src/components/ResponsiveBanner.js
@@ -0,0 +1,9 @@
+import "./ResponsiveBanner.css";
+
+export const ResponsiveBanner = () => {
+  return (
+    <div className="responsive-banner">
+      Watermarko doesn't work well on mobile or tablet devices at the moment.
+    </div>
+  );
+};
